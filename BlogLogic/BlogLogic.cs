2a345adb7110a117abdfe5eae311083d5dd7f188
AlrikAhlstrom <alrik.mattias.ahlstrom@gmail.com>
2016-12-09 13:06:31 +0100

    Add files via upload

---
+++ BlogLogic/BlogLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blogDAL;
+using BlogBDO;
+
+namespace BlogLogic
+{
+    public class PostLogic
+    {
+        PostDAO postDAO = new PostDAO();
+
+        public PostBDO GetPost(int id)
+        {
+            return postDAO.GetPost(id);
+        }
+
+        public bool UpdatePost(ref PostBDO postBDO, ref string message)
+        {
+            var postInDB = GetPost(postBDO.PostId);
+
+            //invalid post to update
+            if (postInDB == null)
+            {
+                message = "cannot get post for this ID";
+                return false;
+            }
+            else
+            {
+                return postDAO.UpdatePost(ref postBDO, ref message);
+            }
+        }
+
+        public bool CreatePost(ref PostBDO postBDO, ref string message)
+        {
+            
+
+                return postDAO.CreatePost(ref postBDO, ref message);
+            
+        }
+    }
+}
