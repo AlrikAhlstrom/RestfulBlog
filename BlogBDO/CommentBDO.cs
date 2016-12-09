257233d75f8f8abbf818681f6d2369224288421f
AlrikAhlstrom <alrik.mattias.ahlstrom@gmail.com>
2016-12-09 13:01:18 +0100

    Add files via upload

---
+++ BlogBDO/CommentBDO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogBDO
+{
+    public class CommentBDO
+    {
+        public int CommentId { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDateTime { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+}
---
+++ BlogBDO/PostBDO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogBDO
+{
+    public class PostBDO
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public DateTime PostDateTime { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+}
